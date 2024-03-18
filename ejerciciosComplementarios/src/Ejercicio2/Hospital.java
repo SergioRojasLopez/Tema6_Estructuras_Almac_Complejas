ad00de75eb269e2fb3db50f59c6c70382b5072aa
SergioRojasLopez <sergiorojaslopez12@gmail.com>
2024-03-18 12:39:13 +0100

    Ejericio 7 Boletin 6.1

--- ejerciciosComplementarios/src/Ejercicio2/Hospital.java
+++ ejerciciosComplementarios/src/Ejercicio2/Hospital.java
@@ -9,46 +9,62 @@ public class Hospital {
 
     private static int ticket = 1;
 
-    LinkedList <Paciente> colaPacientes;
+    LinkedList<Paciente> colaPacientes;
 
     public Hospital() {
         this.colaPacientes = new LinkedList<>();
     }
 
-    public void annadirPaciente(Paciente nuevoPaciente){
+    public void annadirPaciente(Paciente nuevoPaciente) {
         Iterator<Paciente> pa = colaPacientes.iterator();
         int posicion = 0;
         boolean encontrado = false;
 
-        while (pa.hasNext() && !encontrado){
+        while (pa.hasNext() && !encontrado) {
             Paciente paciente1 = pa.next();
-            if (nuevoPaciente.compareTo(paciente1) < 0){
+            if (nuevoPaciente.compareTo(paciente1) < 0) {
                 encontrado = true;
-            }else {
+            } else {
                 posicion++;
             }
-        }colaPacientes.add(posicion,nuevoPaciente);
+        }
+        colaPacientes.add(posicion, nuevoPaciente);
+    }
+
+    public void listadoPacientesOrdenado(Paciente paciente)  {
+
 
     }
 
-    public void listadoPacientesOrdenado (Paciente paciente)throws HospitalException{
-
-
-
-    }
-
-    public List<Paciente> listadoPacientesPrioridad(){
+    public List<Paciente> listadoPacientesPrioridad() {
         return (LinkedList<Paciente>) colaPacientes.clone();
-
     }
 
-    public Paciente atenderPaciente(){
+    public Paciente atenderPaciente() {
         return colaPacientes.poll();
-
     }
 
-    public List<Paciente> listadoPacientesEdad (){
-        return colaPacientes.stream().sorted((p1,p2)-> p2.getEdad() - p1.getEdad()).toList();
+    public List<Paciente> listadoPacientesEdad() {
+        return colaPacientes.stream().sorted((p1, p2) -> p2.getEdad() - p1.getEdad()).toList();
+    }
 
+    public String estadisticasPacientes() {
+        StringBuilder datos = new StringBuilder();
+        for (int i = 0; i < Paciente.MIN_PRIORIDAD; i++){
+            int finalI = i;
+            List<Paciente> pacienteFiltrado = colaPacientes.stream().filter(p->p.getPrioridad() == finalI)
+            datos.append("Hay")
+                    .append(pacienteFiltrado.stream().filter(p->p.getPrioridad() == finalI).count())
+                    .append(" pacientes esperando por prioridad")
+                    .append(i);
+            datos.append("El paciente mas adulto es: ")
+                    .append(pacienteFiltrado.stream().mapToInt(Paciente::getEdad).max())
+                    .append(" y el paciente mas joven es :")
+                    .append(pacienteFiltrado.stream().mapToInt(Paciente::getEdad).min())
+                    .append(" Y la media de edad es: ")
+                    .append(pacienteFiltrado.stream().mapToInt(Paciente::getEdad).average().orElse(0));
+        }
     }
 }
+
+
