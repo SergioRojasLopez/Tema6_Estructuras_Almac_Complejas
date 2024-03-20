2b1cf99820f3a09563be542732209ed2acf4f290
SergioRojasLopez <sergiorojaslopez12@gmail.com>
2024-03-20 17:48:26 +0100

    Practica receta no evaluable

--- ejerciciosComplementarios/src/CartaRestaurante/Principal/Main.java
+++ ejerciciosComplementarios/src/CartaRestaurante/Principal/Main.java
@@ -2,6 +2,7 @@ package CartaRestaurante.Principal;
 
 import CartaRestaurante.Excepciones.RestauranteException;
 import CartaRestaurante.Model.Plato;
+import CartaRestaurante.Model.Restaurante;
 import CartaRestaurante.Utils.MiEntradaSalida;
 
 public class Main {
@@ -10,34 +11,37 @@ public class Main {
 
     public static void main(String[] args) {
 
-        private static void nuevaReceta () {
+        Restaurante r = new Restaurante();
 
-            String nombre = MiEntradaSalida.leerCadena("Nombre de la receta");
-            double duracion = MiEntradaSalida.leerDoublePositivo("Duracion de la receta");
-            Plato plato = new Plato(nombre, duracion);
+    }
 
-            String cadena = null;
-            int cantidad;
+    private static void nuevaReceta() {
 
-            do {
-                cadena = MiEntradaSalida.leerCadena("Introduce el nombre del ingrediente:");
-                if (!cadena.equals(TOKEN_FINALIZADOR)) {
-                    cantidad = MiEntradaSalida.leerEnteroPositivo("Introduzca la cantidad del ingrediente");
-                    plato.addIngrediente(cadena, cantidad);
+        String nombre = MiEntradaSalida.leerCadena("Nombre de la receta");
+        double duracion = MiEntradaSalida.leerDoublePositivo("Duracion de la receta");
+        Plato plato = new Plato(nombre, duracion);
+
+        String cadena = null;
+        int cantidad;
+
+        do {
+            cadena = MiEntradaSalida.leerCadena("Introduce el nombre del ingrediente:");
+            if (!cadena.equals(TOKEN_FINALIZADOR)) {
+                cantidad = MiEntradaSalida.leerEnteroPositivo("Introduzca la cantidad del ingrediente");
+                plato.addIngrediente(cadena, cantidad);
+            }
+        } while (!cadena.equals((TOKEN_FINALIZADOR)));
+        do {
+            cadena = MiEntradaSalida.leerCadena("Introduce el paso a seguir:");
+            if (!cadena.equals(TOKEN_FINALIZADOR)) {
+                try {
+                    plato.addPaso(cadena);
+                } catch (RestauranteException e) {
+                    throw new RuntimeException(e);
                 }
-            } while (!cadena.equals((TOKEN_FINALIZADOR)));
-            do {
-                cadena = MiEntradaSalida.leerCadena("Introduce el paso a seguir:");
-                if (!cadena.equals(TOKEN_FINALIZADOR)) {
-                    try {
-                        plato.addPaso(cadena);
-                    } catch (RestauranteException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            } while (!cadena.equals((TOKEN_FINALIZADOR)));
+            }
+        } while (!cadena.equals((TOKEN_FINALIZADOR)));
 
-            //TODO: Annadir la receta al menu
-        }
+        //TODO: Annadir la receta al menu
     }
 }
